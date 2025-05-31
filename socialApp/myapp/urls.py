5aefff32487599c865be85534e190666d8e7139e
ashim <ashimgautam01@gmail.com>
2025-05-31 23:50:04 +0545

    Add user authentication views and profile template

--- socialApp/myapp/urls.py
+++ socialApp/myapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
     path("",home ,name="home"),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path("accounts/profile/",get_profile , name="profile"),
     path("create/",create_post ,name="create"),
     path("getall/",get_all_post ,name="getall"),
     path('like/<int:id>/', like_post, name='like'),
@@ -10,3 +14,5 @@ urlpatterns = [
     path('get_comment/<int:id>/', get_comment, name='get_comment'),
 
 ]
+
+
